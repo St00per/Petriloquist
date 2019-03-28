5d430a0efad938033c8ccfaf12347bc80ccc20e3
Кирилл Штеффен <kirillshteffen@gmail.com>
2019-03-28 17:10:31 +0600

    init commit

---
+++ Petriloquist/CentralBluetoothManager.swift
@@ -0,0 +1,147 @@
+//
+//  CentralBluetoothManager.swift
+//  Blueduino
+//
+//  Created by Kirill Shteffen on 09/01/2019.
+//  Copyright © 2019 BlackBricks. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+let multiLightCBUUID = CBUUID(string: "0xFFE0")
+let moduleFunctionConfigurationCBUUID = CBUUID(string: "FFE1")
+
+public protocol BluetoothManagerConnectDelegate {
+    func connectingStateSet()
+}
+
+enum DeviceConnectionState {
+    case disconnected
+    case connecting
+    case connected
+}
+
+class CentralBluetoothManager: NSObject {
+    
+    public static let `default` = CentralBluetoothManager()
+    
+    var centralManager: CBCentralManager!
+    var foundDevices: [CBPeripheral] = []
+    var multiLightCharacteristic: CBCharacteristic!
+    
+    
+    var isFirstDidLoad = true
+    var delegate: BluetoothManagerConnectDelegate?
+    
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: DispatchQueue.main)
+    }
+}
+
+extension CentralBluetoothManager: CBCentralManagerDelegate {
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        switch central.state {
+            
+        case .unknown:
+            print("central.state is .unknown")
+        case .resetting:
+            print("central.state is .resetting")
+        case .unsupported:
+            print("central.state is .unsupported")
+        case .unauthorized:
+            print("central.state is .unauthorized")
+        case .poweredOff:
+            print("central.state is .poweredOff")
+        case .poweredOn:
+            print("central.state is .poweredOn")
+            if isFirstDidLoad {
+                centralManager.scanForPeripherals(withServices: [multiLightCBUUID])
+                
+            }
+        }
+
+    }
+    
+    func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        print(error)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        print("Connected!")
+        peripheral.discoverServices(nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        print("Disconnected!")
+    }
+    
+    func connect(peripheral: CBPeripheral) {
+        centralManager.stopScan()
+        print ("Scan stopped, try to connect...")
+        peripheral.delegate = self
+        centralManager.connect(peripheral)
+    }
+    
+    func disconnect(peripheral: CBPeripheral) {
+        centralManager.cancelPeripheralConnection(peripheral)
+    }
+}
+
+extension CentralBluetoothManager: CBPeripheralDelegate {
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        guard let services = peripheral.services else { return }
+        for service in services {
+            print(service)
+            peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+  
+        func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService,
+                        error: Error?) {
+            guard let characteristics = service.characteristics else { return }
+    
+            for characteristic in characteristics {
+                print(characteristic)
+                print(characteristic.properties)
+                }
+            }
+    
+        func peripheral(_ peripheral: CBPeripheral, didWriteValueFor characteristic: CBCharacteristic, error: Error?) {
+            guard error == nil else {
+                print("Error discovering services: error")
+                return
+            }
+            print("Message sent")
+        }
+    
+    func OnOff() -> Data {
+        
+        var dataToWrite = Data()
+        dataToWrite.append(0xE8)
+        dataToWrite.append(0xA1)
+        dataToWrite.append(0x02)
+        
+        return dataToWrite
+    }
+    
+    func frequency1000() -> Data {
+        
+        var dataToWrite = Data()
+        
+        dataToWrite.append(0xE8)
+        dataToWrite.append(0xA2)
+        dataToWrite.append(0x03)
+        dataToWrite.append(0xE8)
+        
+        return dataToWrite
+    }
+}
