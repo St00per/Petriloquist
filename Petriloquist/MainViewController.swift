d7356bf6a8aa7d5dbc694ef25318a24d2e6aa9c4
Кирилл Штеффен <kirillshteffen@gmail.com>
2019-03-29 13:44:45 +0600

    voice recorder implemented

--- Petriloquist/MainViewController.swift
+++ Petriloquist/MainViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import AVFoundation
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, AVAudioRecorderDelegate {
+    
     @IBOutlet weak var connectView: UIView!
     @IBOutlet weak var downloadView: UIView!
     
     @IBOutlet weak var listenButton: UIButton!
     @IBOutlet weak var talkButton: UIButton!
     
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,9 +27,63 @@ class MainViewController: UIViewController {
         
         let tapDownload = UITapGestureRecognizer(target: self, action: #selector(downloadVoice))
         downloadView.addGestureRecognizer(tapDownload)
-    
+        
+        recordingSession = AVAudioSession.sharedInstance()
+        do {
+            try recordingSession.setCategory(.playAndRecord, mode: .default)
+            try recordingSession.setActive(true)
+            recordingSession.requestRecordPermission() { [unowned self] allowed in
+                DispatchQueue.main.async {
+                    if allowed {
+                        //self.loadRecordingUI()
+                    } else {
+                        // failed to record!
+                    }
+                }
+            }
+        } catch {
+            // failed to record!
+        }
+        
     }
     
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    func startRecording() {
+        let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.m4a")
+        
+        let settings = [
+            AVFormatIDKey: Int(kAudioFormatMPEG4AAC),
+            AVSampleRateKey: 12000,
+            AVNumberOfChannelsKey: 1,
+            AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue
+        ]
+        
+        do {
+            audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+            audioRecorder.delegate = self
+            audioRecorder.record()
+            
+            //recordButton.setTitle("Tap to Stop", for: .normal)
+        } catch {
+            finishRecording(success: false)
+        }
+    }
+    
+    func finishRecording(success: Bool) {
+        audioRecorder.stop()
+        audioRecorder = nil
+        
+        if success {
+            //recordButton.setTitle("Tap to Re-record", for: .normal)
+        } else {
+            //recordButton.setTitle("Tap to Record", for: .normal)
+            // recording failed :(
+        }
+    }
     
     @IBAction func startListen(_ sender: UIButton) {
         print("LISTEN PRESSED")
@@ -38,13 +95,15 @@ class MainViewController: UIViewController {
     
     
     @IBAction func startTalk(_ sender: UIButton) {
-        print("TALK PRESSED")
+        startRecording()
+        print("START RECORDING")
     }
     
     @IBAction func stopTalk(_ sender: UIButton) {
-        print("TALK released")
+        finishRecording(success: true)
+        print("STOP RECORDING")
     }
- 
+    
     @objc func connectToDevice() {
         print("Start connecting...")
     }
@@ -52,5 +111,4 @@ class MainViewController: UIViewController {
     @objc func downloadVoice() {
         print("Start downloading...")
     }
- 
 }
